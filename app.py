c5f3babf7b48de528348155a25e014b977067a56
Panneerselvam <panneer.ece2020@gmail.com>
2025-12-05 09:01:10 +0530

    Enable dynamic port configuration for Flask app
    Added environment variable support for port configuration.

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask, request, jsonify
 import pickle
 import numpy as np
+import os
+
+
+
 
 app = Flask(__name__)
 
@@ -59,5 +63,8 @@ def predict():
 
 
 if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
-    app.run(debug=True)
+   
+
